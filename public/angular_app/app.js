e2c2688191d7e85133d7f30a056b37f552048e93
microflyer <microflyer@gmail.com>
2015-01-10 22:51:45 +0800

    completed the angular code with sample static data

--- public/angular_app/app.js
+++ public/angular_app/app.js
@@ -1,9 +1,69 @@
-var controllers = angular.module("controllers", []);
-var services = angular.module("services", []);
-var directives = angular.module("directives", []);
+angular.module('flapperNews', ['ui.router']);
 
-var superSpa = angular.module("superSpa", ["controllers", "services", "directives"]);
+angular.module('flapperNews').config(
+	['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'mainCtrl'
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'postsCtrl'
+		});
 
-controllers.controller("mainCtrl", ["$scope", function($scope) {
-	$scope.greetings = "hello, angular!";
+		$urlRouterProvider.otherwise('home');
+}]);
+
+angular.module('flapperNews').controller('mainCtrl', ['$scope', 'posts', function ($scope, posts) {
+	$scope.posts = posts.posts;
+
+	$scope.addPost = function () {
+		if (!$scope.title || $scope.title === '') {
+			return;
+		}
+
+		$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+		$scope.title = '';
+		$scope.link = '';
+	};
+
+	$scope.incrementUpvotes = function (post) {
+		post.upvotes += 1;
+	};
+
+	$scope.posts.push({
+		title: $scope.title,
+		link: $scope.link,
+		upvotes: 0,
+		comments: [
+			{author: 'Joe', body: 'Cool post!', upvotes: 0},
+			{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+	]});
+
+
+}]);
+
+angular.module('flapperNews').controller('postsCtrl', ['$scope', '$stateParams', 'posts', function ($scope, $stateParams, posts) {
+	$scope.post = posts.posts[$stateParams.id];
+	$scope.addComment = function () {
+		if ($scope.body === '') {
+			return;
+		}
+
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		});
+
+		$scope.body = '';
+	};
+}]);
+
+angular.module('flapperNews').factory('posts', [function () {
+	var o = {posts: []};
+	return o;
 }]);
