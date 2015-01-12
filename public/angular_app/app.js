148954d24da1502df5373f041514cb4345164985
microflyer <microflyer@gmail.com>
2015-01-12 23:24:51 +0800

    Added some comments to app.js of angular

--- public/angular_app/app.js
+++ public/angular_app/app.js
@@ -1,5 +1,7 @@
 angular.module('flapperNews', ['ui.router']);
 
+
+// Route Config
 angular.module('flapperNews').config(
 	['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 		$stateProvider
@@ -7,7 +9,6 @@ angular.module('flapperNews').config(
 			url: '/home',
 			templateUrl: '/home.html',
 			controller: 'mainCtrl',
-			// need more knowledge to understand resolve here
 			resolve: { postPromise: ['posts', function (posts) {
 				return posts.getAll();
 			}]}
@@ -24,6 +25,49 @@ angular.module('flapperNews').config(
 		$urlRouterProvider.otherwise('home');
 }]);
 
+// Service posts
+angular.module('flapperNews').factory('posts', ['$http', function ($http) {
+	var o = { posts: [] };
+
+	o.getAll = function () {
+		return $http.get('/posts').success(function (data) {
+			angular.copy(data, o.posts);
+		});
+	};
+
+	o.create = function (post) {
+		return $http.post('/posts', post).success(function (data) {
+			o.posts.push(data);
+		});
+	};
+
+	o.upvote = function (post) {
+		return $http.put('/posts/' + post._id + '/upvote').success(function (data) {
+			post.upvotes += 1;
+		});
+	};
+
+	o.get = function (id) {
+		return $http.get('/posts/' + id).then(function (res) {
+			return res.data;
+		});
+	};
+
+	o.addComment = function (id, comment) {
+		return $http.post('/posts/' + id + '/comments', comment);
+	};
+
+	o.upvoteComment = function (post, comment) {
+		return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote').success(function (data) {
+			comment.upvotes += 1;
+		});
+	};
+
+	return o;
+}]);
+
+
+// Main Controller
 angular.module('flapperNews').controller('mainCtrl', ['$scope', 'posts', function ($scope, posts) {
 	$scope.posts = posts.posts;
 
@@ -44,8 +88,8 @@ angular.module('flapperNews').controller('mainCtrl', ['$scope', 'posts', functio
 
 }]);
 
+// Post Controller
 angular.module('flapperNews').controller('postsCtrl', ['$scope', '$stateParams', 'posts', 'post', function ($scope, $stateParams, posts, post) {
-	// why can we inject post here? is it because we have it in the resolve object of state config?
 	$scope.post = post;
 	$scope.addComment = function () {
 		if ($scope.body === '') {
@@ -65,46 +109,4 @@ angular.module('flapperNews').controller('postsCtrl', ['$scope', '$stateParams',
 	$scope.incrementUpvotes = function (comment) {
 		posts.upvoteComment(post, comment);
 	}
-}]);
-
-angular.module('flapperNews').factory('posts', ['$http', function ($http) {
-	var o = { posts: [] };
-
-	o.getAll = function () {
-		return $http.get('/posts').success(function (data) {
-			angular.copy(data, o.posts);
-		});
-	};
-
-	o.create = function (post) {
-		return $http.post('/posts', post).success(function (data) {
-			o.posts.push(data);
-		});
-	};
-
-	o.upvote = function (post) {
-		return $http.put('/posts/' + post._id + '/upvote').success(function (data) {
-			post.upvotes += 1;
-			//post.upvotes = data.upvotes
-		});
-	};
-
-	o.get = function (id) {
-		// why use then here? res.data?
-		return $http.get('/posts/' + id).then(function (res) {
-			return res.data;
-		});
-	};
-
-	o.addComment = function (id, comment) {
-		return $http.post('/posts/' + id + '/comments', comment);
-	};
-
-	o.upvoteComment = function (post, comment) {
-		return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote').success(function (data) {
-			comment.upvotes += 1;
-		});
-	};
-
-	return o;
 }]);
