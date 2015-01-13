6f0417a29b8bb92b94d28b524e37068c7a5d0685
microflyer <microflyer@gmail.com>
2015-01-13 11:17:12 +0800

    route example 02: routeProvider api

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -1,12 +1,17 @@
 var demoApp = angular.module("demoApp", ['ngRoute']);
 
 demoApp.config(function($routeProvider){
-	$routeProvider.when('/', {
-		templateUrl: 'angular_playground/views/index.html',
-		controller: 'MainCtrl'
-	});
+	$routeProvider
+						.when('/', {
+							templateUrl: 'angular_playground/views/index.html',
+							controller: 'MainCtrl'
+						})
+						.when('/pizza', {
+							template: 'Yum'
+						})
+						.otherwise({template: "This doesn't exist"});
 });
 
-demoApp.controller('MainCtrl', ['$scope', function ($scope) {
+demoApp.controller('MainCtrl', ['$scope', '$route', function ($scope, $route) {
 	$scope.model = {message: "Hello, Angular Route!"};
 }]);
