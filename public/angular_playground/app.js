db336fcb688d5254fc407663aa02b75f60f9e1de
microflyer <microflyer@gmail.com>
2015-01-13 10:43:16 +0800

    route example 01: ng-view

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -1,1 +1,12 @@
-var demoApp = angular.module("demoApp", []);
+var demoApp = angular.module("demoApp", ['ngRoute']);
+
+demoApp.config(function($routeProvider){
+	$routeProvider.when('/', {
+		templateUrl: 'angular_playground/views/index.html',
+		controller: 'MainCtrl'
+	});
+});
+
+demoApp.controller('MainCtrl', ['$scope', function ($scope) {
+	$scope.model = {message: "Hello, Angular Route!"};
+}]);
