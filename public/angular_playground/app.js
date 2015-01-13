d96a9c07a8fd5fcac94b4f03ae84ad04de8a8c11
microflyer <microflyer@gmail.com>
2015-01-13 17:58:46 +0800

    route example 05b: resolve

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -5,26 +5,40 @@ demoApp.config(function($routeProvider){
 						.when('/', {
 							templateUrl: 'angular_playground/views/index.html',
 							controller: 'MainCtrl',
-              resolve: {username: function ($q, $timeout) {
-                var defered = $q.defer();
-
-                $timeout(function () {
-                  defered.resolve("Danny Jia");
-                }, 3000);
-
-                /*
-                return defered.promise.then(function (data) {
-                  return "Hello, " + data;
-                });
-                */
-
-                return defered.promise;
-              }}
+              resolve: {
+                loadData: mainCtrl.loadData,
+                prepData: mainCtrl.prepData
+              }
 						})
 });
 
-demoApp.controller('MainCtrl', ['$scope', 'username', function ($scope, username) {
+var mainCtrl = demoApp.controller('MainCtrl', ['$scope', '$route', 'loadData', 'prepData',
+                                  function ($scope, $route, loadData, prepData) {
 	$scope.model = {
-    message: username
+    message: "Hello, Angular Route!"
 	};
+
+  console.log($route);
+  console.log(loadData);
+  console.log(prepData);
 }]);
+
+mainCtrl.loadData = function ($q, $timeout) {
+  var defered = $q.defer();
+
+  $timeout(function () {
+    defered.resolve("load data from service");
+  }, 3000);
+
+  return defered.promise;
+}
+
+mainCtrl.prepData = function ($q, $timeout) {
+  var defered = $q.defer();
+
+  $timeout(function () {
+    defered.resolve("prep data from service");
+  }, 3000);
+
+  return defered.promise;
+}
