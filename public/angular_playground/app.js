077c11e760eb8a45249143d8e711e60b1aeff20e
microflyer <microflyer@gmail.com>
2015-01-13 14:50:54 +0800

    first half

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -1,4 +1,25 @@
+var demoApp = angular.module('demoApp', []);
 
+demoApp.factory('data', [function () {
+  return {
+    message: ""
+  };
+}]);
 
+demoApp.filter('reverse', function () {
+  return function (text) {
+    return text.split("").reverse().join("");
+  };
+});
 
+demoApp.controller('FirstCtrl', ['$scope', 'data', function ($scope, data) {
+  $scope.data = data;
+}]);
 
+demoApp.controller('SecondCtrl', ['$scope', 'data', function ($scope, data) {
+  $scope.data = data;
+
+  $scope.reverseString = function (text) {
+    return text.split("").reverse().join("");
+  };
+}]);
