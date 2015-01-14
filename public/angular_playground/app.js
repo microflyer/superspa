2859cdf824d2fb272cddde236ae2f0aa52eee32e
microflyer <microflyer@gmail.com>
2015-01-14 17:49:58 +0800

    promise example

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -1,62 +1,99 @@
-var demoApp = angular.module("demoApp", ['ngRoute']);
+var demoApp = angular.module("demoApp", []);
 
-demoApp.config(function($routeProvider){
-	$routeProvider
-						.when('/', {
-							templateUrl: 'angular_playground/views/index.html',
-							controller: 'MainCtrl'
-						})
-						.when('/pizza', {
-							template: '<h1>Pizza Page: {{pizzaName}}</h1><button class="btn btn-primary" ng-click="changeRoute()">Go Back</button>',
-							controller: 'PizzaCtrl',
-							resolve: {
-								pizzaName: function ($q) {
-									var defered = $q.defer();
+demoApp.controller('MainCtrl', function ($scope, $q, $timeout) {
 
-									//defered.reject("error occured fetching pizza name!");
+	$scope.data = {};
+	$scope.error = "";
 
-									defered.resolve();
+	var getPromise = function () {
+		var defered = $q.defer();
 
-									return defered.promise;
-								}
-							}
-						});
+		$timeout(function () {
+			defered.resolve("Macbook Pro with retina");
+		}, 3000);
+
+		/*
+		$timeout(function () {
+			defered.reject("Failed to get product name from service");
+		}, 2000);
+		*/
+
+		return defered.promise;
+
+	};
+
+	$scope.getProducts = function () {
+		getPromise().then(
+			function (data) {
+				$scope.data.productName = data;
+			},
+			function (errorMsg) {
+				$scope.error = errorMsg;
+			});
+	};
+
+
+
+
+	var getA = function () {
+		var defered = $q.defer();
+
+		$timeout(function () {
+			defered.resolve("A is returned.");
+		}, 1500);
+
+		return defered.promise;
+	}
+
+	var getBbyA = function (a) {
+		var defered = $q.defer();
+
+		$timeout(function () {
+			defered.resolve("B is returned.");
+		}, 1500);
+
+		return defered.promise;
+	}
+
+	var getCbyB = function (b) {
+		var defered = $q.defer();
+
+		$timeout(function () {
+			defered.resolve("C is returned.");
+		}, 1500);
+
+		return defered.promise;
+	}
+
+	var getDbyC = function (c) {
+		var defered = $q.defer();
+
+		$timeout(function () {
+			defered.resolve("D is returned.");
+		}, 1500);
+
+		return defered.promise;
+	}
+
+	$scope.promiseChains = function () {
+
+		$scope.data.messages = [];
+
+		getA()
+		.then(function (data) {
+			$scope.data.messages.push(data + "Now getting B... ");
+			return getBbyA(data);
+		})
+		.then(function (data) {
+			$scope.data.messages.push(data + "Now getting C... ");
+			return getCbyB(data);
+		})
+		.then(function (data) {
+			$scope.data.messages.push(data + "Now getting D... ");
+			return getDbyC(data);
+		})
+		.then(function (data) {
+			$scope.data.messages.push(data);
+		})
+	}
 });
-
-demoApp.controller('MainCtrl', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
-	$scope.model = {
-    message: "Hello, Angular Route!"
-	};
-
-	$scope.changeRoute = function () {
-		$location.path('/pizza');
-	};
-
-	$rootScope.$on('$routeChangeStart', function (event, current, previous) {
-		console.log("route change starts...");
-		console.log(event);
-		console.log(current);
-		console.log(previous);
-	});
-
-	$rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
-		console.log("route change succeeded...");
-		console.log(event);
-		console.log(current);
-		console.log(previous);
-	})
-
-	$rootScope.$on('$routeChangeError', function(event, current, previous, rejectMsg) {
-		console.log("route change error...")
-		console.log(rejectMsg);
-	});
-
-}]);
-
-demoApp.controller('PizzaCtrl', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
-
-	$scope.changeRoute = function () {
-		$location.path('/');
-	};
-
-}]);
