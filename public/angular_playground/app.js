3a5773dd0897a9583f66cf8a661cf913e0beb5b4
microflyer <microflyer@gmail.com>
2015-01-18 03:32:23 +0800

    EX07: trasclusion basics

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -1,58 +1,14 @@
 var demoApp = angular.module("demoApp", []);
 
-demoApp.directive('superhero', function () {
+demoApp.directive('panel', function () {
   return {
     restrict: "E",
-
-    controller: function ($scope) {
-      $scope.abilities = [];
-
-      this.addStrength = function () {
-        $scope.abilities.push("strength");
-      };
-
-      this.addSpeed = function () {
-        $scope.abilities.push("speed");
-      };
-
-      this.addFlight = function () {
-        $scope.abilities.push("flight");
-      }
-    },
+    transclude: true,
+    replace: true,
+    template: '<div class="alert alert-info ng-transclude" role="alert"></div>',
 
     link: function (scope, element) {
-      element.bind("mouseenter", function () {
-        console.log(scope.abilities);
-      });
+
     }
   };
 });
-
-demoApp.directive("strength", function () {
-  return {
-    require: "superhero",
-    link: function (scope, element, attrs, superheroCtrl) {
-      superheroCtrl.addStrength();
-    }
-  };
-});
-
-demoApp.directive("speed", function () {
-  return {
-    require: "superhero",
-    link: function (scope, element, attrs, superheroCtrl) {
-      superheroCtrl.addSpeed();
-    }
-  };
-});
-
-demoApp.directive("flight", function () {
-  return {
-    require: "superhero",
-    link: function (scope, element, attrs, superheroCtrl) {
-      superheroCtrl.addFlight();
-    }
-  };
-});
-
-// the next step is to introduce isolate scope a little.
