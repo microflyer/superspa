75bdfb55d02cf8a9a2c0e96cf13e4d79886c2789
microflyer <microflyer@gmail.com>
2015-01-17 16:48:31 +0800

    EX06: directive to directive communication

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -1,22 +1,58 @@
 var demoApp = angular.module("demoApp", []);
 
-demoApp.controller("MainCtrl", function ($scope) {
-  $scope.loadMoreTweets = function () {
-    alert('Loading tweets...');
-  };
-
-  $scope.deleteTweets = function () {
-    alert('Deleting tweets...');
-  };
-});
-
-demoApp.directive("enter", function () {
+demoApp.directive('superhero', function () {
   return {
-    restrict: 'A',
-    link: function (scope, element, attrs) {
-      element.bind('mouseenter', function () {
-        scope.$apply(attrs.enter);
+    restrict: "E",
+
+    controller: function ($scope) {
+      $scope.abilities = [];
+
+      this.addStrength = function () {
+        $scope.abilities.push("strength");
+      };
+
+      this.addSpeed = function () {
+        $scope.abilities.push("speed");
+      };
+
+      this.addFlight = function () {
+        $scope.abilities.push("flight");
+      }
+    },
+
+    link: function (scope, element) {
+      element.bind("mouseenter", function () {
+        console.log(scope.abilities);
       });
     }
   };
 });
+
+demoApp.directive("strength", function () {
+  return {
+    require: "superhero",
+    link: function (scope, element, attrs, superheroCtrl) {
+      superheroCtrl.addStrength();
+    }
+  };
+});
+
+demoApp.directive("speed", function () {
+  return {
+    require: "superhero",
+    link: function (scope, element, attrs, superheroCtrl) {
+      superheroCtrl.addSpeed();
+    }
+  };
+});
+
+demoApp.directive("flight", function () {
+  return {
+    require: "superhero",
+    link: function (scope, element, attrs, superheroCtrl) {
+      superheroCtrl.addFlight();
+    }
+  };
+});
+
+// the next step is to introduce isolate scope a little.
