57a5e8400e114035d121fdc6669571e1b8bfd843
microflyer <microflyer@gmail.com>
2015-01-17 09:45:47 +0800

    EX01: first directive

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -1,1 +1,8 @@
 var demoApp = angular.module("demoApp", []);
+
+demoApp.directive('superman', function () {
+  return {
+    restrict: 'E',
+    template:'<h1>Here I am to save the day</h1>'
+  }
+})
