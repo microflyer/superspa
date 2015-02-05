4d579a9b1ca18f5c17894f2f46f78cd1faa340ea
microflyer <microflyer@gmail.com>
2015-02-05 14:32:58 +0800

    EX01: watch

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -1,1 +1,10 @@
 var demoApp = angular.module("demoApp", []);
+
+demoApp.controller('MainCtrl', ['$scope', function ($scope) {
+  $scope.message = "Hello, Angular!";
+
+  setTimeout(function () {
+      $scope.message = "Angular is awesome!"
+  }, 3000);
+
+}]);
