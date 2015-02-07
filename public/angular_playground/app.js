37357807d09e443a90be85e4d2ffc90f346cec39
microflyer <microflyer@gmail.com>
2015-02-07 19:26:47 +0800

    EX02: scope apply

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -2,16 +2,9 @@ var demoApp = angular.module("demoApp", []);
 
 demoApp.controller('MainCtrl', function ($scope) {
 
-  $scope.messages = [];
+  $scope.message = "Hello, AngularJS!";
 
-  $scope.$watch('user.password', function (newVal, oldVal) {
-
-    $scope.messages.length = 0;
-
-    if (angular.isDefined(newVal)) {
-      if (newVal.length < 5) {
-        $scope.messages.push("Password must have at least 4 characters");
-      }
-    }
-  })
+  setTimeout(function () {
+      $scope.message = "AngularJS is Awesome!";
+  }, 3000);
 });
