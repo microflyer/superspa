4ce45ec4e5fd7660dfe007e5aa7526c2ecdf9249
microflyer <microflyer@gmail.com>
2015-02-07 13:02:41 +0800

    EX01: scope watch

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -1,1 +1,17 @@
 var demoApp = angular.module("demoApp", []);
+
+demoApp.controller('MainCtrl', function ($scope) {
+
+  $scope.messages = [];
+
+  $scope.$watch('user.password', function (newVal, oldVal) {
+
+    $scope.messages.length = 0;
+
+    if (angular.isDefined(newVal)) {
+      if (newVal.length < 5) {
+        $scope.messages.push("Password must have at least 4 characters");
+      }
+    }
+  })
+});
