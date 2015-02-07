4eb850390489450f31beb9fea5b51c46bbe43402
microflyer <microflyer@gmail.com>
2015-02-07 20:31:29 +0800

    EX03: scope emit broadcast

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -1,10 +1,32 @@
 var demoApp = angular.module("demoApp", []);
 
-demoApp.controller('MainCtrl', function ($scope) {
+demoApp.controller('ParentCtrl', function ($scope) {
+  $scope.messages = [];
+  $scope.text = "";
 
-  $scope.message = "Hello, AngularJS!";
+  $scope.sendMessage = function (text) {
+    $scope.$broadcast("messagefromparent", {source: "Parent", text: text});
+    $scope.text = "";
+  };
 
-  setTimeout(function () {
-      $scope.message = "AngularJS is Awesome!";
-  }, 3000);
+  $scope.$on("messagefromChild", function (event, message) {
+    console.log(event);
+    $scope.messages.push(message);
+  })
+
+});
+
+demoApp.controller('ChildCtrl', function ($scope) {
+  $scope.messages = [];
+  $scope.text = "";
+
+  $scope.sendMessage = function (text) {
+    $scope.$emit("messagefromChild", {source: "Child", text: text});
+    $scope.text = "";
+  };
+
+  $scope.$on("messagefromparent", function (event, message) {
+    console.log(event);
+    $scope.messages.push(message);
+  })
 });
