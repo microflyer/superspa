54bae2f9f82d698047a73c22ffb886c77832693a
microflyer <microflyer@gmail.com>
2015-02-09 16:21:58 +0800

    http service in depth

--- public/angular_playground/app.js
+++ public/angular_playground/app.js
@@ -1,10 +1,66 @@
 var demoApp = angular.module("demoApp", []);
 
-demoApp.controller('MainCtrl', ['$scope', function ($scope) {
-  $scope.message = "Hello, Angular!";
+demoApp.config(function ($httpProvider) {
 
-  setTimeout(function () {
-      $scope.message = "Angular is awesome!"
-  }, 3000);
+  console.log($httpProvider);
 
+  angular.forEach($httpProvider.defaults.transformRequest, function (value, index) {
+    console.log(value.toString());
+  });
+
+  angular.forEach($httpProvider.defaults.transformResponse, function (value, index) {
+    console.log(value.toString());
+  });
+
+  $httpProvider.interceptors.push("authInterceptor");
+});
+
+demoApp.controller('MainCtrl', ['$scope', '$http', 'authService', function ($scope, $http, authService) {
+
+  $scope.getPeople = function () {
+    $http.get('/people').success(
+      function (data) {
+        console.log(data)
+      }
+    ).error(function (data, status) {
+      console.log(data);
+      console.log(status);
+    });
+  };
+
+  $scope.authorizeUser = function () {
+    authService.authorize("username1", "password1");
+  };
 }]);
+
+demoApp.factory('authService', function ($q, $http) {
+  return {
+    isAuthorized: false,
+    authToken: "",
+    authorize: function (username, password) {
+
+      var deferred = $q.defer();
+
+      $http.post('/authorize', {username: username, password: password}).success(function (data) {
+        this.isAuthorized = true;
+        this.authToken = data.authorizaionToken;
+        deferred.resolve();
+      });
+
+      return deferred.promise;
+    }
+  };
+});
+
+demoApp.factory('authInterceptor', function(authService) {
+  return {
+    request: function (config) {
+      if (authService.isAuthorized) {
+        config.headers['authorizaion-token'] = authService.authToken;
+      }
+
+      return config;
+
+    }
+  };
+});
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+function validateUser(token) {
+  if (token === "Ajut9kGNByipqwABZ") {
+    return true;
+  }
+
+  return false;
+}
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
@@ -8,6 +16,19 @@ router.get('/', function(req, res) {
 
 router.get('/playground', function(req, res) {
   res.render('playground', { title: 'Playground' });
+});
+
+router.get('/people', function (req, res, next) {
+  var authToken = req.get('authorizaion-token');
+  if (validateUser(authToken)) {
+    res.json([{name: "Neil", age: 32}, {name: "Jill", age: 30}]);
+  } else {
+    res.status(401).end();
+  }
+});
+
+router.post('/authorize', function (req, res, next) {
+  res.json({authorizaionToken: "Ajut9kGNByipqwABZ"});
 });
 
 module.exports = router;
