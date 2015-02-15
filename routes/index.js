33bd7030569b245825138646545145eacb486d51
microflyer <microflyer@gmail.com>
2015-02-15 23:44:47 +0800

    added reactjs

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'AngularJS' });
 });
 
 router.get('/playground', function(req, res) {
-  res.render('playground', { title: 'Playground' });
+  res.render('playground', { title: 'AngularJS Playground' });
+});
+
+router.get('/reactindex', function(req, res) {
+  res.render('reactindex', { title: 'ReactJS' });
+});
+
+router.get('/reactplayground', function(req, res) {
+  res.render('reactplayground', { title: 'ReactJS Playground' });
 });
 
 module.exports = router;
